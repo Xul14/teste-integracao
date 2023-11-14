39e88d37eb59d95eb9afe317ce2ff092d1b682b6
JULIA SOARES DE ALMEIDA <22282188@dsn127m1242315.cfp127aluno.local>
2023-11-14 15:20:57 -0300

    Testes de GET

--- api.test.js
+++ api.test.js
@@ -19,10 +19,42 @@ describe("Testes de Integração da API com JSON Server", () => {
 
   });
 
+  test("Deve buscar todas as dicas", async () => {
+    const response = await app.get('/v1/saveeats/dicas');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Object);
+
+  });
+
+  test("Deve buscar todas as receitas", async () => {
+    const response = await app.get('/v1/saveeats/receitas');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Object);
+
+  });
+
+  test("Deve buscar todas os pedidos", async () => {
+    const response = await app.get('/v1/saveeats/pedido');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Object);
+
+  });
+
+  test("Deve buscar todas as avaliações", async () => {
+    const response = await app.get('/v1/saveeats/avaliacao');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Object);
+
+  });
+
   test("Deve criar um novo usuário", async () => {
     const novoUsuario = {
       nome: "Nick",
-      email: "nickinho@gmail.com",
+      email: "kiki@gmail.com",
       senha: "Temporaria1",
       cpf: "0909-0987",
       foto: "",
@@ -37,6 +69,20 @@ describe("Testes de Integração da API com JSON Server", () => {
     }
 
     const response = await app.post("/v1/saveeats/cadastro/cliente").send(novoUsuario);
+
+    expect(response.status).toBe(201);
+  });
+
+    test("Deve criar uma nova avaliação", async () => {
+    const novaAvaliacao = {
+      quantidade_estrela: 4,
+      descricao: "Minha avaliacao é 4 pq demorou dms",
+      data_avaliacao: "2023/09/10",
+      id_restaurante: 5,
+      id_cliente: 6,
+    }
+
+    const response = await app.post("/v1/saveeats/avaliacao").send(novaAvaliacao);
 
     expect(response.status).toBe(201);
   });
