bc7e01bcfa468e88c0e4f48911c50569e28d5aa0
Julia <juliasoaresdealmeidaj@gmail.com>
2023-11-14 10:29:58 -0300

    # Testes de integração

--- api.test.js
+++ api.test.js
@@ -1,81 +1,45 @@
 const request = require("supertest");
-const app = request("http://localhost:8080");
+const app = request("http://localhost:3000");
 
 describe("Testes de Integração da API com JSON Server", () => {
+
   test("Deve buscar todos os usuários", async () => {
+    const response = await app.get('/v1/saveeats/clientes');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Object);
+
+  });
+
+  test("Deve buscar todos os restaurantes", async () => {
     const response = await app.get('/v1/saveeats/restaurantes');
 
     expect(response.status).toBe(200);
     expect(response.body).toBeInstanceOf(Object);
+
   });
-  
 
   test("Deve criar um novo usuário", async () => {
-    const novoUsuario = { nome: "Charlie", email: "charlie@example.com" };
+    const novoUsuario = {
+      nome: "Nick",
+      email: "nickinho@gmail.com",
+      senha: "Temporaria1",
+      cpf: "0909-0987",
+      foto: "",
+      telefone: "11 95656-6531",
+      cep: "0989-000",
+      logradouro: "Rua Constantino",
+      complemento: "Próximo ao centro",
+      bairro: "Vale do Constantino Real",
+      localidade: "Barueri",
+      numero: 40,
+      uf: "SP"
+    }
 
-    const response = await app.post("/usuarios").send(novoUsuario);
+    const response = await app.post("/v1/saveeats/cadastro/cliente").send(novoUsuario);
 
     expect(response.status).toBe(201);
-    expect(response.body.nome).toBe(novoUsuario.nome);
   });
 
-  it('Deve buscar um usuário pelo ID', async () => {
-    
-    const novoUsuario = { nome: 'Fernanda', email: 'fernanda@example.com' };
-    const criacaoResponse = await app
-      .post('/usuarios')
-      .send(novoUsuario);
-
-    const usuarioId = criacaoResponse.body.id;
-
-    
-    const buscaResponse = await app.get(`/usuarios/${usuarioId}`);
-
-    expect(buscaResponse.status).toBe(200);
-
-    expect(buscaResponse.body.id).toBe(usuarioId);
-    expect(buscaResponse.body.nome).toBe(novoUsuario.nome);
-    expect(buscaResponse.body.email).toBe(novoUsuario.email);
-  });
-
-  test("Deve excluir um usuário existente", async () => {
-    
-    const novoUsuario = { nome: "Leonid", email: "leonid@gmail.com" };
-    const criacaoResponse = await app.post("/usuarios").send(novoUsuario);
-
-    const usuarioId = criacaoResponse.body.id;
-
-    const deleteResponse = await app.delete(`/usuarios/${usuarioId}`);
-
-    expect(deleteResponse.status).toBe(200);
-
-    const buscaResponse = await app.get("/usuarios");
-    const usuarioExcluido = buscaResponse.body.find(
-      (user) => user.id === usuarioId
-    );
-    expect(usuarioExcluido).toBeUndefined();
-  });
-
-  test('Deve atualizar os dados de um usuário existente', async () => {
-
-    const novoUsuario = { nome: 'Eva', email: 'eva@gmail.com' };
-    const criacaoResponse = await app
-      .post('/usuarios')
-      .send(novoUsuario);
-
-    const usuarioId = criacaoResponse.body.id;
-
-    const novosDados = { nome: 'Eva Novo', email: 'evanovo@gmail.com' };
-    const updateResponse = await app
-      .put(`/usuarios/${usuarioId}`)
-      .send(novosDados);
-
-    expect(updateResponse.status).toBe(200);
-
-
-    const buscaResponse = await app.get(`/usuarios/${usuarioId}`);
-    expect(buscaResponse.body.nome).toBe(novosDados.nome);
-    expect(buscaResponse.body.email).toBe(novosDados.email);
-  });
 
 });
